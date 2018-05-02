25a2213600264e192bc9625d64d9469d5f4a489b
Andreas Bade <badeand@Andreass-MacBook-Pro.local>
2018-05-02 19:42:54 +0200

    sim settings and improved docker + doc

--- dockerdevserver/dockerdevstart.sh
+++ dockerdevserver/dockerdevstart.sh
@@ -1,5 +1,8 @@
 cd /badenvision/socketserver/server/
 nohup npm start &
+echo waiting
+sleep 5
 cd /badenvision/hubserver/server/
 nohup npm start &
+cd /badenvision/socketserver/server/
 tail -f nohup.out
